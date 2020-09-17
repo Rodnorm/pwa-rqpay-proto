c5ed54f2e9a6cfee4e909eadff7eaf3f7d02f027
Rodrigo Normando <r.normando@hotmail.com>
2020-09-17 15:14:23 +0200

    finished implementation

--- src/views/FeedbackTab.tsx
+++ src/views/FeedbackTab.tsx
@@ -1,23 +1,52 @@
-import { CircularProgress, Container, createStyles, makeStyles, Theme } from "@material-ui/core";
-import React from "react";
+import {
+  CircularProgress,
+  Container,
+  createStyles,
+  makeStyles,
+  Theme,
+  Typography,
+} from "@material-ui/core";
+import React, { useState } from "react";
 import { GeneralProps } from "../services/payment.service";
+import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
       display: "flex",
-      justifyContent: "center"
-    }
+      alignItems: "center",
+      flexDirection: "column",
+    },
+    iconSuccess: {
+      color: "green",
+      fontSize: "5rem",
+    },
+    fadeIn: {
+      opacity: 0,
+      animation: "$fadeIn 1.5s forwards",
+    },
+    "@keyframes fadeIn": {
+      "100%": {
+        opacity: 1,
+      },
+    },
   })
 );
 
 function FeedbackTab(props: GeneralProps) {
-  const classes = useStyles()
-  const {} = props;
-
+  const classes = useStyles();
+  const [positiveFeedback, setPositiveFeedback] = useState(false);
+  setTimeout(() => setPositiveFeedback(true), 1000);
   return (
     <Container className={classes.container}>
-      <CircularProgress color="inherit" />
+      {positiveFeedback ? (
+        <div className={`${classes.container} ${classes.fadeIn}`}>
+          <CheckCircleOutlineIcon className={classes.iconSuccess} />
+          <Typography>All good!</Typography>
+        </div>
+      ) : (
+        <CircularProgress color="inherit" />
+      )}
     </Container>
   );
 }
--- src/views/WelcomeTab.tsx
+++ src/views/WelcomeTab.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
-import { GeneralProps } from '../services/payment.service';
+import {
+  Container,
+  createStyles,
+  makeStyles,
+  Theme,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+import { GeneralProps } from "../services/payment.service";
+import logo from "../assets/logo.png";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    image: {
+      height: "150px",
+      width: "150px",
+      marginBottom: "2rem"
+    },
+    container: {
+      opacity: 0,
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      animation: "$fadeIn 1.5s forwards",
+    },
+    "@keyframes fadeIn": {
+      "100%": {
+        opacity: 1,
+      },
+    },
+  })
+);
 
 function WelcomeTab(props: GeneralProps) {
-  const {} = props;
-
-  return <div>Payment Made Simple</div>;
+  const classes = useStyles();
+  return (
+    <Container className={classes.container}>
+      <img className={classes.image} src={logo} alt="Our Logo" />
+      <Typography>Payment Made Simple</Typography>
+    </Container>
+  );
 }
 
-export default WelcomeTab   
+export default WelcomeTab;
