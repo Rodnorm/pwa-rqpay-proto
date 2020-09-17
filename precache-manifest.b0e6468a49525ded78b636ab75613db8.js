7a7290b8092d8d1aba31cfeaebf5d9e39dfbb306
Rodrigo Normando <r.normando@hotmail.com>
2020-09-17 13:41:04 +0200

    Updates

---
+++ precache-manifest.b0e6468a49525ded78b636ab75613db8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "137f2f0f9f5444b39ec9647fc459aad3",
+    "url": "/pwa-rqpay-proto/index.html"
+  },
+  {
+    "revision": "7cb0240e1c96ad946141",
+    "url": "/pwa-rqpay-proto/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e9e48d7cf49ae90b8934",
+    "url": "/pwa-rqpay-proto/static/js/2.e3e6368c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pwa-rqpay-proto/static/js/2.e3e6368c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cb0240e1c96ad946141",
+    "url": "/pwa-rqpay-proto/static/js/main.99c46851.chunk.js"
+  },
+  {
+    "revision": "880bff0d04ee51acf4f6",
+    "url": "/pwa-rqpay-proto/static/js/runtime-main.e902ec84.js"
+  }
+]);
